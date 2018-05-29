a6106cb70e29276e41fabf64543e3354b909d30e
tomommm <tomo0819>
2018-05-29 16:42:28 +0900

    [upgrade]カメラ制御（マウス操作の追加）
    MyCameraスクリプトにマウス操作の機能の追加をしました

--- JGA2018_A/Assets/Stage/Main/Scripts/MyCamera.cs
+++ JGA2018_A/Assets/Stage/Main/Scripts/MyCamera.cs
@@ -17,88 +17,57 @@ using UnityEngine;
 /// カメラを制御するクラス（仮）
 /// </summary>
 public class MyCamera : MonoBehaviour {
-
-    /// <summary>
-    /// カメラの対象
-    /// </summary>
     [SerializeField]
-    Transform m_target = null;
-    /// <summary>
-    /// 座標
-    /// </summary>
+    Transform m_player;//対象のプレイヤー
     [SerializeField]
-    float m_speed = 0.0f;
+    float distance = 1.2f;//対象プレイヤーからカメラを離す距離
+    [SerializeField]
+    Quaternion vRotation;//カメラの垂直回転
+    [SerializeField]
+    Quaternion hRotation;//カメラの平行回転
+    [SerializeField]
+    float m_ypos=1.0f;//高さ
+    [SerializeField]
+    Vector3 velocity;
 
-    float m_posx = 0.0f;
-    float m_posy = 0.0f;
-    float m_posz = 0.0f;
+    [SerializeField]
+    float turnSpeed = 10.0f;
 
-    public Transform Target
+    void Start()
     {
-        get { return m_target; }
+        //回転の初期化
+        vRotation = Quaternion.Euler(0, 0, 0);//垂直回転(x軸を軸とする回転)
+        hRotation = Quaternion.identity;//水平回転(y軸を軸とする回転)は、無回転
+        transform.rotation = hRotation * vRotation;//最終的なカメラの回転は垂直回転してから水平回転する合成回転
+
+        //位置の初期化
+        //player位置から距離distanceだけ手前に引いた位置を設定する
+        transform.position = m_player.position - transform.rotation * Vector3.forward * distance;
+
     }
-
-    Transform m_cameraTransform = null;
-    Transform m_pivot = null;
-
-    void Awake()
-    {
-        Camera camera = GetComponentInChildren<Camera>();
-        Debug.AssertFormat(camera != null, "カメラがありません");
-        if (camera == null)
-        {
-            return;
-        }
-
-        m_cameraTransform = camera.transform;
-        m_pivot = m_cameraTransform.parent;
-    }
-
     void LateUpdate()
     {
-        UpdateCamera();
+        //水平カメラの更新
+        hRotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * turnSpeed, 0);
+        //カメラの回転(transform.rotation)の更新
+        //方法１：垂直回転してから水平回転する合成回転とする
+        transform.rotation = vRotation * hRotation;
+
+            //カメラの位置(transform.position)の更新
+            //player位置から距離distanceだけ手前に引いた位置を設定
+            transform.position = m_player.position - transform.rotation * Vector3.forward * distance;
+        transform.position = new Vector3(transform.position.x, m_player.transform.position.y + m_ypos, transform.position.z);
+
+        if (velocity.magnitude > 0)
+        {
+            //プレイヤーの回転の更新(transform.rotation)の更新
+            //無回転状態ののプレイヤーのZ+方向(後頭部)を、移動の反対方向(-velocity)に回す回転とします
+            transform.rotation = Quaternion.LookRotation(-velocity);
+
+            // プレイヤーの位置(transform.position)の更新
+            // 移動方向ベクトル(velocity)を足し込みます
+            transform.position += velocity;
+        }
     }
 
-    void UpdateCamera()
-    {
-        if (Target == null)
-        {
-            return;
-        }
-        Vector3 targetPos = Target.position;
-        targetPos.y += 2.0f;
-        targetPos.z -= 2.5f;
-        transform.position = targetPos;
-        float deltaSpeed = m_speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, m_speed);
-    }
-
-    [ContextMenu("ApplyTarget")]
-    void ApplyForceTarget()
-    {
-        if (Target == null)
-        {
-            return;
-        }
-        transform.position = Target.position;
-        SetCameraTransform();
-
-        if (m_cameraTransform == null)
-        {
-            return;
-        }
-        m_cameraTransform.transform.LookAt(Target);
-    }
-
-    void SetCameraTransform()
-    {
-        Camera camera = GetComponentInChildren<Camera>();
-        Debug.AssertFormat(camera != null, "カメラがありません2");
-        if (camera == null)
-        {
-            return;
-        }
-        m_cameraTransform = camera.transform;
-        m_pivot = m_cameraTransform.parent;
-    }
 }

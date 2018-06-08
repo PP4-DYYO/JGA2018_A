30b4fc2747d13df0bd823ede235c95498ff40570
ifeat <ifeatablems@outlook.jp>
2018-06-08 14:51:06 +0900

    [add]攻撃の威力の追加及びMyPlayerの整理
    インスペクタ上で見やすくなるようにするため

---
+++ JGA2018_A/Assets/Stage/Main/Scripts/MyAttack.cs
@@ -0,0 +1,27 @@
+﻿//----------------------------------------------------------------------------------------------------
+//
+//2018年6月8日～
+//作成者　京都コンピュータ学院京都駅前校　ゲーム学科　4回生　奥田裕也
+//編集者
+//
+//----------------------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 攻撃
+/// </summary>
+public class MyAttack : MonoBehaviour
+{
+	/// <summary>
+	/// 威力
+	/// </summary>
+	int m_power;
+	public int Power
+	{
+		get { return m_power; }
+		set { m_power = value; }
+	}
+}

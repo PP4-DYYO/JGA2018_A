939da539829fa84ac5305f663a2798491f255831
tomommm <tomo0819>
2018-07-10 14:31:28 +0900

    [upgrade]カメラの制御方法更新とコーディング改変
    カメラがプレイヤーとの間に壁を挟むと壁の前にカメラが移動するように変更
    コーディング規約にのっとったコードの改変を行った

--- JGA2018_A/Assets/Stage/Main/Scripts/MyCamera.cs
+++ JGA2018_A/Assets/Stage/Main/Scripts/MyCamera.cs
@@ -5,21 +5,17 @@
 //
 ////////////////////////////////////////////////////////////
 
-
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 /// <summary>
 /// カメラを制御するクラス（仮）
 /// </summary>
-
-//カメラ属性を付ける
 [RequireComponent(typeof(Camera))]
-public class MyCamera : MonoBehaviour {
+public class MyCamera : MonoBehaviour
+{
     /// <summary>
     /// ゲーム
     /// </summary>
@@ -30,88 +26,135 @@ public class MyCamera : MonoBehaviour {
         get { return myGame; }
     }
 
-    //カメラの対象
-    [SerializeField]
-    Transform player;
-    // カメラとプレイヤーとの距離[m]
-    [SerializeField]
-    float DistanceToPlayerM = 2f;
-    // カメラを横にスライドさせる；プラスの時右へ，マイナスの時左へ[m]
-    [SerializeField]
-    float slideDistanceM = 0f;  
-    // 注視点の高さ[m]
-    [SerializeField]
-    float HeightM = 1.2f;  
-    // 感度
-    [SerializeField]
-    float rotationSensitivity = 100f;
-    float rotX = 0.0f;
-    float rotY = 0.0f;
+    /// <summary>
+    /// カメラの対象
+    /// </summary>
+    MyPlayer m_player;
 
-    float duration = 3;
+    [Header("カメラとプレイヤーの距離")]
+    /// <summary>
+    /// カメラとプレイヤーとの距離[m]
+    /// </summary>
+    [SerializeField]
+    float m_distanceToPlayerM = 2f;
 
+    [Header("注視点の高さ")]
+    /// <summary>
+    /// 注視点の高さ[m]
+    /// </summary>
+    [SerializeField]
+    float m_heightM;
+
+    [Header("カメラ感度")]
+    /// <summary>
+    /// 感度
+    /// </summary>
+    [SerializeField]
+    float m_rotationSensitivity;
+
+    /// <summary>
+    /// Xの回転量
+    /// </summary>
+    float m_rotX;
+
+    /// <summary>
+    /// Yの回転量
+    /// </summary>
+    float m_rotY;
+
+    /// <summary>
+    /// プレイヤーの中心位置
+    /// </summary>
+    Vector3 m_lookAt;
+
+    /// <summary>
+    /// Rayの方向
+    /// </summary>
+    Vector3 m_rayDirection;
+
+    /// <summary>
+    /// Rayの作成
+    /// </summary>
+    Ray m_ray;
+
+    /// <summary>
+    /// Rayが衝突したコライダーの情報を得る
+    /// </summary>
+    RaycastHit m_hit;
+
+    //----------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// 初期
+    /// </summary>
     void Start()
     {
-        if (player == null)
+        if (m_player == null)
         {
             Debug.LogError("ターゲットが設定されていない");
             Application.Quit();
         }
+
+        m_player = myGame.CharacterScript.PlayerScript;
     }
 
+    //----------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// 定期フレーム
+    /// </summary>
     void FixedUpdate()
     {
-        rotX = Input.GetAxis("HorizontalR") * Time.deltaTime * rotationSensitivity;
-        rotY = Input.GetAxis("VerticalR") * Time.deltaTime * rotationSensitivity;
+        m_rotX = Input.GetAxis("HorizontalR") * Time.deltaTime * m_rotationSensitivity;
+        m_rotY = Input.GetAxis("VerticalR") * Time.deltaTime * m_rotationSensitivity;
 
-
-        var lookAt = player.position + Vector3.up * HeightM;
+        m_lookAt = m_player.transform.position + Vector3.up * m_heightM;
 
         // 回転
-        transform.RotateAround(lookAt, Vector3.up, rotX);
+        transform.RotateAround(m_lookAt, Vector3.up, m_rotX);
         // カメラがプレイヤーの真上や真下にあるときにそれ以上回転させないようにする
-        if (transform.forward.y > 0.9f && rotY < 0)
+        if (transform.forward.y > 0.9f && m_rotY < 0)
         {
-            rotY = 0;
+            m_rotY = 0;
         }
-        if (transform.forward.y < -0.9f && rotY > 0)
+        if (transform.forward.y < -0.9f && m_rotY > 0)
         {
-            rotY = 0;
+            m_rotY = 0;
         }
-        transform.RotateAround(lookAt, transform.right, rotY);
-
-
-        // カメラとプレイヤーとの間の距離を調整
-        transform.position = lookAt - transform.forward * DistanceToPlayerM;
+        transform.RotateAround(m_lookAt, transform.right, m_rotY);
 
         // 視点の設定
-        transform.LookAt(lookAt);
+        transform.LookAt(m_lookAt);
 
-        // カメラを横にずらして中央を開ける
-        transform.position = transform.position + transform.right * slideDistanceM;
+        // カメラとプレイヤーとの間の距離を調整
+        transform.position = m_lookAt - transform.forward * m_distanceToPlayerM;
+
+        CheckWall();
     }
 
-        // 敵とプレイヤーとの間に壁があるかを確認する
-        void update()
+    //----------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// 敵とプレイヤーとの間に壁があるかを確認する
+    /// </summary>
+    void CheckWall()
+    {
+        // 自分の位置とプレイヤーの位置から向きベクトルを作成しRayに渡す
+        m_rayDirection = transform.position - m_lookAt;
+        m_ray = new Ray(m_lookAt, m_rayDirection);
+
+        // Rayが衝突したかどうか
+        if (Physics.Raycast(m_ray, out m_hit, m_distanceToPlayerM))
         {
-            //Rayの長さ
-            float maxDistance = 8.0f;
-            // Rayの作成
-            // 自分の位置とプレイヤーの位置から向きベクトルを作成しRayに渡す
-            Vector3 direction = (player.transform.position - transform.position).normalized;
-            Ray ray = new Ray(transform.position, direction);
-            // Rayが衝突したコライダーの情報を得る
-            RaycastHit hit;
-
-            // Rayの可視化
-            Debug.DrawRay(ray.origin, ray.direction * maxDistance, Color.red);
-            // Rayが衝突したかどうか
-            if (Physics.Raycast(ray, out hit, maxDistance))
-            {
-                if (hit.collider.tag == "Player")
-                {
-                   
-                }
-            }
+            transform.position = m_hit.point;
         }
+        transform.position -= m_ray.GetPoint(Camera.main.nearClipPlane) - m_lookAt;
     }
+
+    //----------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// カメラの位置を設定
+    /// </summary>
+    /// <param name="relativeLocations">相対的位置</param>
+    public void SetPosition(Vector3 relativeLocations)
+    {
+        transform.position = m_player.transform.position + relativeLocations;
+    }
+}

641d7f559a75f75ce4eb80e35dd1d1d39bcbd68a
JunkiYoshida <jun_y0726@yahoo.co.jp>
2018-08-02 12:07:10 +0900

    [add]大臣の特殊技演出用のプレファブ追加
    ウイルスの毒とキャリーのワープ

---
+++ JGA2018_A/Assets/Stage/Main/Scripts/MyWarpSign.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyWarpSign : MonoBehaviour {
+
+    Light m_spotLight;
+
+	// Use this for initialization
+	void Start () {
+        m_spotLight = gameObject.GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        m_spotLight.spotAngle += 3.0f;
+        m_spotLight.intensity -= 0.2f;
+    }
+}
